dd3d04ae520a005265d3b9b4c55b499e336f333e
Nahuel149 <nahuelbalsas199@gmail.com>
2025-05-02 23:19:25 +0900

    feat: Add new changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ const router = require('express').Router();
 const authRoutes = require('./auth.routes');
 const userRoutes = require('./user.routes');
 const testRoutes = require('./test.routes');
-
 const shopRoutes = require('./shop.routes');
+const aiRoutes = require('./ai.routes');
 
 // Rutas de autenticación (/api/auth/*)
 router.use('/auth', authRoutes);
@@ -13,6 +13,9 @@ router.use('/users', userRoutes);
 
 // Rutas de tienda (/api/shops/*)
 router.use('/shops', shopRoutes);
+
+// Rutas de IA (/api/ai/*)
+router.use('/ai', aiRoutes);
 
 // Rutas de prueba (/api/test/*)
 router.use('/test', testRoutes);
